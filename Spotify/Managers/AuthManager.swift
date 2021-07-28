747fec2d354f7519c3293dbf2b3f035ba7b02c44
Mikalai Bekliamishchau <mikalai.bekliamishchau@innowise-group.com>
2021-07-28 11:03:10 +0300

    Implement auth logic.

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -11,7 +11,19 @@ final class AuthManager {
 
     static let shared = AuthManager()
 
+    struct Constants {
+        static let clientId = "194cd1795c5e4317a045d5a05fcb4b61"
+        static let clientSecret = "730fbae634924fdc955a3bea8f9090a3"
+    }
     private init() {}
+
+    public var signInURL: URL? {
+        let redirectURI = "https://www.linkedin.com/in/nikolai-beklemishchev-1420651b4/"
+        let scopes = "user-read-private"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientId)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
 
     var isSignedIn: Bool {
         return false
@@ -31,5 +43,17 @@ final class AuthManager {
 
     private var shouldRefreshToken: Bool {
         return false
+    }
+
+    public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)) {
+        // Get Token
+    }
+    
+    public func refreshAccessToken() {
+
+    }
+
+    private func cashToken() {
+
     }
 }
